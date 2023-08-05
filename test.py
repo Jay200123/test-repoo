442f92b3d0ec5049b9f07751c011f09f24a6ace8
Jay200123 <kenken.ayonon@gmail.com>
2023-08-05 22:03:43 +0800

    "commit stash and pop"

--- test.py
+++ test.py
@@ -7,3 +7,12 @@ print("Hello World Gab")
 print("Testing Git Stash")
 
 print("Purpose of stash and pop")
+
+print("Testing Stash and Pop")
+
+number = 12
+number2 = 12
+
+total = number + number2
+print(total)
+
